426cf04de681c8839b43683f6dcf31107ddc7df0
epomatti <evandro.pomatti@gmail.com>
2022-04-06 17:10:18 -0300

    theme

--- components/widget.tsx
+++ components/widget.tsx
@@ -1,4 +1,4 @@
-import { darkTheme, lightTheme, Theme, SwapWidget } from '@uniswap/widgets';
+import { darkTheme, lightTheme, SwapWidget } from '@uniswap/widgets';
 import '@uniswap/widgets/fonts.css';
 import { useEffect, useState } from 'react';
 
@@ -7,30 +7,25 @@ declare var window: any;
 // Infura endpoint
 const jsonRpcEndpoint = process.env.REACT_APP_POLYGON_NODE;
 
-const getSystemTheme = () => {
-  
-}
-
 const Widget = () => {
-
   const [provider, setProvider] = useState();
   useEffect(() => {
     setProvider(window.ethereum);
   }, []);
 
-  const [theme, setTheme] = useState(darkTheme);
+  const [theme, setTheme] = useState(lightTheme);
   useEffect(() => {
-    setTheme(theme);
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userTheme = isDarkMode ? darkTheme : lightTheme;
+    setTheme(userTheme);
   }, [theme]);
-
-  let darkMode = true
 
   return <div className="Uniswap">
     <SwapWidget
       provider={provider}
       jsonRpcEndpoint={jsonRpcEndpoint}
       width={450}
-      theme={darkMode ? darkTheme : lightTheme}
+      theme={theme}
     />
   </div>
 }
